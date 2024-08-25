48bf9f2d0c25bdb15f38b8d29bc4cd07255cef6e
Raymon Roos <raymon.roos@hotmail.com>
2024-08-25 13:36:50 +0200

    style: add blank lines to `stylix.nix`

--- stylix.nix
+++ stylix.nix
@@ -5,9 +5,12 @@
 }: {
   stylix = {
     enable = true;
+
     image = /home/ray/files/pictures/wallpapers/elden_ring_tarnished_warrior.jpg;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/snazzy.yaml";
+
     polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/snazzy.yaml";
+
     fonts = {
       sizes.terminal = 9;
       sizes.applications = 11;
@@ -17,6 +20,7 @@
       sansSerif = config.stylix.fonts.monospace;
       emoji = config.stylix.fonts.monospace;
     };
+
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
