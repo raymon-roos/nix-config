a003117b9f8804fce824a2fd36a2709c5312a2a3
Raymon Roos <raymon.roos@hotmail.com>
2024-11-30 12:50:04 +0100

    feat(home/raydesk): more settings for librewolf config
    Includes default search behaviour and do-not-track-me header.

--- home-manager/raydesk/home.nix
+++ home-manager/raydesk/home.nix
@@ -29,8 +29,12 @@
       cmus
       hledger
       hledger-ui
+      id3v2
+      puddletag
       remind
       thunderbird
+      vesktop
+      vorbis-tools
       wl-clipboard-rs
       ytfzf
     ];
@@ -47,11 +51,20 @@
       enable = true;
       settings = {
         "identity.fxaccounts.enabled" = true;
+        "privacy.donottrackheader.enabled" = true;
         "privacy.resistFingerprinting" = true;
         "privacy.clearOnShutdown.history" = true;
         "privacy.clearOnShutdown.downloads" = true;
+        "browser.contentblocking.category" = "strict";
         "network.http.referer.XOriginPolicy" = 2; # Referer header lets websites know how you got there. 2 for same-host-only
         "webgl.disabled" = false;
+
+        "browser.startup.homepage" = "https://search.rhscz.eu/";
+        "browser.urlbar.suggest.engines" = false;
+        "browser.urlbar.suggest.history" = false;
+        "browser.urlbar.suggest.openpage" = false;
+        "browser.urlbar.suggest.recentsearches" = false;
+        "layout.css.devPixelsPerPx" = "0.70"; # shrink ui
       };
       languagePacks = ["en-GB" "nl"];
     };
