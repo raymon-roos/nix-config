8f4f9735f729a188c3cf23b8d0ebd1751af37f1a
Raymon Roos <raymon.roos@hotmail.com>
2024-12-07 12:52:34 +0100

    feat(home/common): centralize direnv-nix project cache directories
    By default, direnv creates a `.direnv/` directory at the root of your
    project which you'll have to add to the .gitignore, even if you're the
    only one on your team that's using direnv(-nix). This shell hook instead
    puts all these project caches under `$XDG_CACHE_HOME/direnv/layouts`,
    and does some work to prevent name collisions just to be safe.

--- home-manager/common/dev/default.nix
+++ home-manager/common/dev/default.nix
@@ -11,5 +11,16 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    stdlib = ''
+      declare -A direnv_layout_dirs
+      direnv_layout_dir() {
+          local hash path
+          echo "''${direnv_layout_dirs[$PWD]:=$(
+              hash="$(sha1sum - <<< "$PWD" | head -c20)"
+              path="''${PWD//[^a-zA-Z0-9]/-}"
+              echo "''${XDG_CACHE_HOME}/direnv/layouts/$hash$path"
+          )}"
+      }
+    '';
   };
 }
