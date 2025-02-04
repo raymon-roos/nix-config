3056ed730bfe67a9b20492f0e7c8744c56beaf09
Raymon Roos <raymon.roos@hotmail.com>
2025-02-04 21:53:43 +0100

    refactor(home/common): apply shellcheck suggestion on functions.sh
    Remove `function` declaration keywords, this is a bashism and not
    needed.

--- home-manager/common/shell/functions.sh
+++ home-manager/common/shell/functions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-function des() {
+des() {
     # First argument should be an existing project name,
     # depends on dev environment conventions used at work
     # If no argument was given, use the current directory
@@ -8,31 +8,31 @@ function des() {
     docker exec -it php-"${1:-$current_dir}" php artisan db
 }
 
-function dep() {
+dep() {
     # enter interactive shell for php container, using cwd as name
     current_dir="$(basename "$PWD")"
     docker exec -it php-"${1:-$current_dir}" bash
 }
 
-function dex() {
+dex() {
     # enter interactive shell for given container, using cwd as name
     current_dir="$(basename "$PWD")"
     docker exec -it "$1"-"${2:-$current_dir}" bash
 }
 
-function psg() {
+psg() {
     ps -aux | rg --color=always -i "($1|USER.*PID %CPU)" | rg --color=always -vi 'rg'
 }
 
-function sshagent() {
+sshagent() {
     eval "$(keychain --dir "$XDG_STATE_HOME/keychain" --eval id_ed25519)"
 }
 
-function volset() {
+volset() {
     pactl set-sink-volume "${2:-@DEFAULT_SINK@}" "$1"
 }
 
-function volget() {
+volget() {
     pactl get-sink-volume "${1:-@DEFAULT_SINK@}"
 }
 
