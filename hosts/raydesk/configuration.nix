70f9adf305f647a12730ec895302c3a1cad65e57
Raymon Roos <raymon.roos@hotmail.com>
2025-02-08 12:18:22 +0100

    feat(system/raydesk): configure host SSH keys, but keep service disabled
    Some agenix (and maybe nix-sops?) require ssh host keys, but I don't
    want to keep the service running permanently.

--- hosts/raydesk/configuration.nix
+++ hosts/raydesk/configuration.nix
@@ -71,6 +71,22 @@
       enable = true;
       pulse.enable = true;
     };
+    openssh = {
+      enable = false;
+      startWhenNeeded = true;
+      hostKeys = [
+        {
+          comment = "raydesk system";
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
+      settings = {
+        PermitRootLogin = "no";
+        X11Forwarding = false;
+        PasswordAuthentication = false;
+      };
+    };
   };
 
   security.polkit.enable = true;
