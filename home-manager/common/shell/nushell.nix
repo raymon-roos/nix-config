1875fa71997ea486960baef97cd5dd214d9c5ed7
Raymon Roos <raymon.roos@hotmail.com>
2025-04-23 23:25:05 +0200

    feat(home/common): manually integrate `fzf` into nushell
    Based on an open issue from the `fzf` github, adapted to nix.

--- home-manager/common/shell/nushell.nix
+++ home-manager/common/shell/nushell.nix
@@ -13,6 +13,54 @@ in {
       show_banner = false;
       use_kitty_protocol = config.programs.kitty.enable;
       history.file_format = "sqlite";
+      keybindings = lib.mkIf config.programs.fzf.enable [
+        # Adapted from https://github.com/junegunn/fzf/issues/4122
+        {
+          name = "fzf_cd";
+          modifier = "alt";
+          keycode = "char_c";
+          mode = ["emacs" "vi_insert" "vi_normal"];
+          event = {
+            send = "executehostcommand";
+            cmd = ''
+              let result = nu -l -i -c $env.FZF_ALT_C_COMMAND | fzf
+              cd $result
+            '';
+          };
+        }
+        {
+          name = "fzf_complete";
+          modifier = "control";
+          keycode = "char_t";
+          mode = ["emacs" "vi_insert"];
+          event = {
+            send = "executehostcommand";
+            cmd = ''
+              let result = nu -l -i -c $env.FZF_CTRL_T_COMMAND | fzf
+              commandline edit --append $result
+              commandline set-cursor --end
+            '';
+          };
+        }
+        {
+          name = "fzf_history";
+          modifier = "control";
+          keycode = "char_r";
+          mode = ["emacs" "vi_insert" "vi_normal"];
+          event = {
+            send = "executehostcommand";
+            cmd = ''
+              let result = history
+                | sort-by -r start_timestamp
+                | get command
+                | to text
+                | fzf --preview "echo {}" --preview-window "up:3:wrap"
+              commandline edit --append $result
+              commandline set-cursor --end
+            '';
+          };
+        }
+      ];
     };
 
     shellAliases = {
