e3913094b03e4ca7df3f1ff3813e1677780c0a5d
Raymon Roos <raymon.roos@hotmail.com>
2025-05-20 19:12:04 +0200

    feat(home/common): auto-select only remaining mako notification acion

--- home-manager/common/wayland/hyprland.nix
+++ home-manager/common/wayland/hyprland.nix
@@ -138,7 +138,7 @@ with lib; {
             "$mainMod CONTROL, M, exec, cmus-remote -C 'toggle aaa_mode'"
 
             "$mainMod, L, exec, makoctl dismiss"
-            "$mainMod, U, exec, makoctl menu $menu"
+            "$mainMod, U, exec, makoctl menu $menu --accept-single"
             "$mainMod, Y, exec, makoctl restore"
 
             "$mainMod, return, exec, $terminal"
