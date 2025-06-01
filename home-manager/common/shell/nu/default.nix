56ba036125fa80bd7a51302002a037c8ee4f6c99
Raymon Roos <raymon.roos@hotmail.com>
2025-06-01 19:32:50 +0200

    feat(home/common/nu): add toggle-sort bind to nushell fzf history widget

--- home-manager/common/shell/nu/default.nix
+++ home-manager/common/shell/nu/default.nix
@@ -62,7 +62,12 @@ in {
                 | query db "select command_line from history group by command_line order by start_timestamp desc;"
                 | get command_line
                 | str join (char -i 0)
-                | fzf --read0 --query (commandline) --scheme history --preview '"{r}" | nu-highlight' --preview-window "up:4:wrap"
+                | fzf --read0
+                  --query=(commandline)
+                  --scheme=history
+                  --bind="ctrl-r:toggle-sort"
+                  --preview-window="up:4:wrap"
+                  --preview='"{r}" | nu-highlight'
               commandline edit --replace $result
               commandline set-cursor --end
             '';
