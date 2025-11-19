4eec266f76771d15212f497aa769eb11e5b1ec55
Raymon Roos <raymon.roos@hotmail.com>
2025-11-19 10:35:02 +0100

    fix(home/common/dev): go module options changed & install golangci-lint

--- home-manager/common/dev/go.nix
+++ home-manager/common/dev/go.nix
@@ -7,19 +7,25 @@
 with lib; {
   options.common.dev.go.enable = mkEnableOption "Go dev tools";
 
-  config = mkIf config.common.dev.go.enable {
-    programs.go = {
-      enable = true;
-      goBin = ".xdg/local/bin";
-      goPath = ".xdg/local/share/go";
-      telemetry.mode = "off";
-    };
+  config = let
+    inherit (config.xdg) dataHome;
+  in
+    mkIf config.common.dev.go.enable {
+      programs.go = {
+        enable = true;
+        telemetry.mode = "off";
+        env = {
+          GOPATH = ["${dataHome}/go"];
+          GOBIN = ".xdg/local/bin";
+        };
+      };
 
-    home.packages = with pkgs; [
-      gopls
-      revive
-      gofumpt
-      air
-    ];
-  };
+      home.packages = with pkgs; [
+        gopls
+        revive
+        gofumpt
+        golangci-lint
+        air
+      ];
+    };
 }
