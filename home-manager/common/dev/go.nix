0db87063256ec733d74f5492b06ef44c01824d61
Raymon Roos <raymon.roos@hotmail.com>
2026-01-11 16:41:01 +0100

    refactor(home/common/dev): inline the redundant `let in` in Go config

--- home-manager/common/dev/go.nix
+++ home-manager/common/dev/go.nix
@@ -7,25 +7,22 @@
 with lib; {
   options.common.dev.go.enable = mkEnableOption "Go dev tools";
 
-  config = let
-    inherit (config.xdg) dataHome;
-  in
-    mkIf config.common.dev.go.enable {
-      programs.go = {
-        enable = true;
-        telemetry.mode = "off";
-        env = {
-          GOPATH = ["${dataHome}/go"];
-          GOBIN = ".xdg/local/bin";
-        };
+  config = mkIf config.common.dev.go.enable {
+    programs.go = {
+      enable = true;
+      telemetry.mode = "off";
+      env = {
+        GOPATH = ["${config.xdg.dataHome}/go"];
+        GOBIN = ".xdg/local/bin";
       };
-
-      home.packages = with pkgs; [
-        gopls
-        revive
-        gofumpt
-        golangci-lint
-        air
-      ];
     };
+
+    home.packages = with pkgs; [
+      gopls
+      revive
+      gofumpt
+      golangci-lint
+      air
+    ];
+  };
 }
