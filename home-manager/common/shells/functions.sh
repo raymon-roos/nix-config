231c3e04e0c62cb4e6f9802666e9c081f941aad6
Raymon Roos <raymon.roos@hotmail.com>
2026-01-27 17:15:41 +0100

    feat(home/common/shells): update old bash functions
    These were some of the oldest shell configuration I've had. I made them
    for use in my first internship. Since then I've had more experience with
    docker, and these functions seem kind of dumb now. I don't exclusively
    use PHP anymore, and would rather use Alpine as a base image wherever
    possible, so `bash` isn't even always available in the container.
    I also don't use `pactl` since moving away from Artix, so use `pwctl`
    instead.

--- home-manager/common/shells/functions.sh
+++ home-manager/common/shells/functions.sh
@@ -1,25 +1,3 @@
-#!/usr/bin/env sh
-
-des() {
-    # First argument should be an existing project name,
-    # depends on dev environment conventions used at work
-    # If no argument was given, use the current directory
-    current_dir="$(basename "$PWD")"
-    docker exec -it php-"${1:-$current_dir}" php artisan db
-}
-
-dep() {
-    # enter interactive shell for php container, using cwd as name
-    current_dir="$(basename "$PWD")"
-    docker exec -it php-"${1:-$current_dir}" bash
-}
-
-dex() {
-    # enter interactive shell for given container, using cwd as name
-    current_dir="$(basename "$PWD")"
-    docker exec -it "$1"-"${2:-$current_dir}" bash
-}
-
 psg() {
     ps -aux | rg --color=always -i "($1|USER.*PID %CPU)" | rg --color=always -vi 'rg'
 }
@@ -29,9 +7,9 @@ sshagent() {
 }
 
 volset() {
-    pactl set-sink-volume "${2:-@DEFAULT_SINK@}" "$1"
+    wpctl set-volume "${2:-@DEFAULT_SINK@}" "$1"
 }
 
 volget() {
-    pactl get-sink-volume "${1:-@DEFAULT_SINK@}"
+    wpctl get-volume "${1:-@DEFAULT_SINK@}"
 }
