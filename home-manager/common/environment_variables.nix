ddd5b3f6216a10c4318603962fd443f2290e8bef
Raymon Roos <raymon.roos@hotmail.com>
2026-02-09 09:48:06 +0100

    fix(home/common): custom xdg userDirs were removed from env vars
    It used to be the case that `xdg.userDirs.extraConfig` entries were
    automatically added as environment variables, and I depended on this
    behaviour for some of my aliases. So now I add them manually.

--- home-manager/common/environment_variables.nix
+++ home-manager/common/environment_variables.nix
@@ -1,24 +1,26 @@
 {config, ...}: let
   inherit (config.xdg) cacheHome configHome dataHome stateHome;
 in {
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    BROWSER = "librewolf";
-    TERMINAL = "kitty";
-    MANPAGER = "nvim '+Man!'";
-    MANWIDTH = 90;
+  home.sessionVariables =
+    {
+      EDITOR = "nvim";
+      BROWSER = "librewolf";
+      TERMINAL = "kitty";
+      MANPAGER = "nvim '+Man!'";
+      MANWIDTH = 90;
 
-    ADB_KEYS_PATH = "${configHome}/android";
-    CALCHISTFILE = "${stateHome}/calc/calc_history";
-    IMAPFILTER_HOME = "${configHome}/imapfilter";
-    LESSHISTFILE = "${stateHome}/less/lesshst";
-    LYNX_CFG_PATH = "${configHome}/lynx/lynx.cfg";
-    MYSQL_HISTFILE = "${stateHome}/mysql/mysql_history";
-    NOTMUCH_CONFIG = "${configHome}/notmuch/config";
-    TEXMFCONFIG = "${configHome}/texlive/texmf-config";
-    TEXMFHOME = "${dataHome}/texmf";
-    TEXMFVAR = "${cacheHome}/texlive/texmf-var";
-    WGETRC = "${configHome}/wget/wgetrc";
-    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${configHome}/java";
-  };
+      ADB_KEYS_PATH = "${configHome}/android";
+      CALCHISTFILE = "${stateHome}/calc/calc_history";
+      IMAPFILTER_HOME = "${configHome}/imapfilter";
+      LESSHISTFILE = "${stateHome}/less/lesshst";
+      LYNX_CFG_PATH = "${configHome}/lynx/lynx.cfg";
+      MYSQL_HISTFILE = "${stateHome}/mysql/mysql_history";
+      NOTMUCH_CONFIG = "${configHome}/notmuch/config";
+      TEXMFCONFIG = "${configHome}/texlive/texmf-config";
+      TEXMFHOME = "${dataHome}/texmf";
+      TEXMFVAR = "${cacheHome}/texlive/texmf-var";
+      WGETRC = "${configHome}/wget/wgetrc";
+      _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${configHome}/java";
+    }
+    // config.xdg.userDirs.extraConfig;
 }
