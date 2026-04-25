dfc69affeadc9fe44a9f286d48bee40a30c8713e
Raymon Roos <raymon.roos@hotmail.com>
2026-04-25 18:14:33 +0200

    !feat(home/common/librewolf): xdg base directories for librewolf
    This requires manual migration of `~/.librewolf` to
    `$XDG_CONFIG_HOME/librewolf/librewolf`. Only it cannot be simply moved,
    that breaks the browser so that it won't load any pages anymore.
    
    I borked my profile and the backup I made, so I ended up recreating it,
    loosing all my addon data. I restored a Ublock Origin backup from my
    phone.
    
    I'm not sure what the procedure is to carry over the profile data from
    one location to another succesfully.

--- home-manager/common/librewolf/default.nix
+++ home-manager/common/librewolf/default.nix
@@ -21,100 +21,111 @@ with lib; {
     ./preferences.nix
   ];
 
-  config = mkIf config.common.librewolf.enable {
-    programs.librewolf = let
-      inherit (config.programs.librewolf) settings;
-      containers = {
-        bitacademy = {
-          id = 2;
-          color = "green";
-        };
-        discord = {
-          id = 3;
-          color = "purple";
-        };
-        github = {
-          id = 4;
-          color = "blue";
-        };
-        hu = {
-          id = 5;
-          color = "turquoise";
-        };
-        banking = {
-          id = 6;
-          color = "green";
-        };
-        google = {
-          id = 7;
-          color = "red";
-        };
-        atlassian = {
-          id = 8;
-          color = "red";
-        };
-        private = {
-          id = 9;
-          color = "toolbar";
-        };
-        shopping = {
-          id = 10;
-          color = "purple";
-        };
-        microsoft = {
-          id = 11;
-          color = "purple";
-        };
+  config = let
+    inherit (config.xdg) configHome;
+  in
+    mkIf config.common.librewolf.enable {
+      home.file = {
+        ".librewolf/librewolf.overrides.cfg".target = "${configHome}/librewolf/librewolf/librewolf.overrides.cfg";
+        ".librewolf/native-messaging-hosts/.keep".enable = false;
+        ".mozilla/native-messaging-hosts/.keep".enable = false;
+        # ".librewolf/native-messaging-hosts/.keep".target = "${configHome}/librewolf/native-messaging-hosts/.keep";
       };
-      search = {
-        # home-manager will always fail to rebuild with this off due to
-        # a file conflict. Will overwrite existing search configurations.
-        force = true;
-        default = "searx";
-        privateDefault = "searx";
-        engines = {
-          "searx" = {
-            urls = [{template = "https://search.rhscz.eu/search?q={searchTerms}";}];
-            definedAliases = ["s"];
+
+      programs.librewolf = let
+        inherit (config.programs.librewolf) settings;
+        containers = {
+          bitacademy = {
+            id = 2;
+            color = "green";
           };
-          "ddglite" = {
-            urls = [{template = "https://lite.duckduckgo.com/lite/";}];
-            definedAliases = ["ddg"];
+          discord = {
+            id = 3;
+            color = "purple";
           };
-          "mynixos" = {
-            urls = [{template = "https://mynixos.com/search?q={searchTerms}";}];
-            definedAliases = ["my"];
+          github = {
+            id = 4;
+            color = "blue";
           };
-          "noogle.dev" = {
-            urls = [{template = "https://noogle.dev/q?term={searchTerms}";}];
-            definedAliases = ["no"];
+          hu = {
+            id = 5;
+            color = "turquoise";
           };
-          "wiki.nixos" = {
-            urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
-            definedAliases = ["wix"];
+          banking = {
+            id = 6;
+            color = "green";
           };
-          "github" = {
-            urls = [{template = "https://github.com/search?q={searchTerms}";}];
-            definedAliases = ["gh"];
+          google = {
+            id = 7;
+            color = "red";
           };
-          "discogs" = {
-            urls = [{template = "https://www.discogs.com/search?q={searchTerms}";}];
-            definedAliases = ["dis"];
+          atlassian = {
+            id = 8;
+            color = "red";
+          };
+          private = {
+            id = 9;
+            color = "toolbar";
+          };
+          shopping = {
+            id = 10;
+            color = "purple";
+          };
+          microsoft = {
+            id = 11;
+            color = "purple";
           };
         };
-        order = ["searx" "ddglite" "github" "mynixos" "discogs"];
-      };
-    in {
-      enable = true;
-      package = pkgs.librewolf-bin;
-      languagePacks = ["en-GB" "nl"];
-      profiles = mkIf config.common.librewolf-advanced.enable {
-        default = {
-          isDefault = true;
-          containersForce = true;
-          inherit search containers settings;
+        search = {
+          # home-manager will always fail to rebuild with this off due to
+          # a file conflict. Will overwrite existing search configurations.
+          force = true;
+          default = "searx";
+          privateDefault = "searx";
+          engines = {
+            "searx" = {
+              urls = [{template = "https://search.rhscz.eu/search?q={searchTerms}";}];
+              definedAliases = ["s"];
+            };
+            "ddglite" = {
+              urls = [{template = "https://lite.duckduckgo.com/lite/";}];
+              definedAliases = ["ddg"];
+            };
+            "mynixos" = {
+              urls = [{template = "https://mynixos.com/search?q={searchTerms}";}];
+              definedAliases = ["my"];
+            };
+            "noogle.dev" = {
+              urls = [{template = "https://noogle.dev/q?term={searchTerms}";}];
+              definedAliases = ["no"];
+            };
+            "wiki.nixos" = {
+              urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
+              definedAliases = ["wix"];
+            };
+            "github" = {
+              urls = [{template = "https://github.com/search?q={searchTerms}";}];
+              definedAliases = ["gh"];
+            };
+            "discogs" = {
+              urls = [{template = "https://www.discogs.com/search?q={searchTerms}";}];
+              definedAliases = ["dis"];
+            };
+          };
+          order = ["searx" "ddglite" "github" "mynixos" "discogs"];
+        };
+      in {
+        enable = true;
+        package = pkgs.librewolf-bin;
+        configPath = "${configHome}/librewolf/librewolf";
+        languagePacks = ["en-GB" "nl"];
+        profiles = mkIf config.common.librewolf-advanced.enable {
+          default = {
+            isDefault = true;
+            containersForce = true;
+            inherit search containers settings;
+          };
         };
       };
     };
-  };
 }
